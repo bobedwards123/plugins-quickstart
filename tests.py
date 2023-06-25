3c2bc9684783c07fe77c68c1e88cb60906f26495
leolukaz123 <122065825+leolukaz123@users.noreply.github.com>
2023-06-25 21:45:15 +1000

    simplified the code

--- tests.py
+++ tests.py
@@ -2,123 +2,8 @@ print('running tests for the project')
 
 # Path: main.py
 from utils import create_presentation_new
-from pptx import Presentation
-import io
-from pptx.util import Pt
-
-dictionary = {
-  "slide_data": [
-    {
-      "heading": "Introduction to Dogs",
-      "body": "Dogs, also known as Canis lupus familiaris, are domesticated mammals that are commonly kept as pets. They are known for their loyalty and companionship.",
-      "layout": 2
-    },
-    {
-      "heading": "History of Dogs",
-      "body": "Dogs were likely the first domesticated animals, with evidence suggesting domestication as early as 15,000 years ago. They have been bred over generations for various behaviors, sensory capabilities, and physical attributes.",
-      "layout": 2
-    },
-     {
-      "heading": "History of Dogs",
-      "body": "Dogs were likely the first domesticated animals, with evidence suggesting domestication as early as 15,000 years ago. They have been bred over generations for various behaviors, sensory capabilities, and physical attributes.",
-      "layout": 2
-    },
-     {
-      "heading": "History of Dogs",
-      "body": "Dogs were likely the first domesticated animals, with evidence suggesting domestication as early as 15,000 years ago. They have been bred over generations for various behaviors, sensory capabilities, and physical attributes.",
-      "layout": 3
-    },
-    {
-      "heading": "History of Dogs",
-      "body": "Dogs were likely the first domesticated animals, with evidence suggesting domestication as early as 15,000 years ago. They have been bred over generations for various behaviors, sensory capabilities, and physical attributes.",
-      "layout": 3
-    },
-    {
-      "heading": "History of Dogs",
-      "body": "Dogs were likely the first domesticated animals, with evidence suggesting domestication as early as 15,000 years ago. They have been bred over generations for various behaviors, sensory capabilities, and physical attributes.",
-      "layout": 3
-    },
-    {
-      "heading": "History of Dogs",
-      "body": "Dogs were likely the first domesticated animals, with evidence suggesting domestication as early as 15,000 years ago. They have been bred over generations for various behaviors, sensory capabilities, and physical attributes.",
-      "layout": 3
-    },
-    {
-      "heading": "History of Dogs",
-      "body": "Dogs were likely the first domesticated animals, with evidence suggesting domestication as early as 15,000 years ago. They have been bred over generations for various behaviors, sensory capabilities, and physical attributes.",
-      "layout": 3
-    },
-    {
-      "heading": "History of Dogs",
-      "body": "Dogs were likely the first domesticated animals, with evidence suggesting domestication as early as 15,000 years ago. They have been bred over generations for various behaviors, sensory capabilities, and physical attributes.",
-      "layout": 3
-    },
-    {
-      "heading": "History of Dogs",
-      "body": "Dogs were likely the first domesticated animals, with evidence suggesting domestication as early as 15,000 years ago. They have been bred over generations for various behaviors, sensory capabilities, and physical attributes.",
-      "layout": 3
-    },
-    {
-      "heading": "History of Dogs",
-      "body": "Dogs were likely the first domesticated animals, with evidence suggesting domestication as early as 15,000 years ago. They have been bred over generations for various behaviors, sensory capabilities, and physical attributes.",
-      "layout": 3
-    },
-    {
-      "heading": "History of Dogs",
-      "body": "Dogs were likely the first domesticated animals, with evidence suggesting domestication as early as 15,000 years ago. They have been bred over generations for various behaviors, sensory capabilities, and physical attributes.",
-      "layout": 3
-    },
-    {
-      "heading": "History of Dogs",
-      "body": "Dogs were likely the first domesticated animals, with evidence suggesting domestication as early as 15,000 years ago. They have been bred over generations for various behaviors, sensory capabilities, and physical attributes.",
-      "layout": 3
-    },
-    {
-      "heading": "History of Dogs",
-      "body": "Dogs were likely the first domesticated animals, with evidence suggesting domestication as early as 15,000 years ago. They have been bred over generations for various behaviors, sensory capabilities, and physical attributes.",
-      "layout": 3
-    },
-    {
-      "heading": "History of Dogs",
-      "body": "Dogs were likely the first domesticated animals, with evidence suggesting domestication as early as 15,000 years ago. They have been bred over generations for various behaviors, sensory capabilities, and physical attributes.",
-      "layout": 3
-    },
-    {
-      "heading": "History of Dogs",
-      "body": "Dogs were likely the first domesticated animals, with evidence suggesting domestication as early as 15,000 years ago. They have been bred over generations for various behaviors, sensory capabilities, and physical attributes.",
-      "layout": 3
-    },
-    {
-      "heading": "History of Dogs",
-      "body": "Dogs were likely the first domesticated animals, with evidence suggesting domestication as early as 15,000 years ago. They have been bred over generations for various behaviors, sensory capabilities, and physical attributes.",
-      "layout": 3
-    }
-  ]
-}
 
 
-# it should return a file path
-# print("it should create a presentation")
-# output_link = create_presentation(dictionary["slide_data"])
-# print(output_link)
-
-# # assert that the file path is correct with .pptx at the end
-# assert output_link[-5:] == ".pptx"
-
-# # it should delete a presentation
-# print("it should delete a presentation")
-# delete_presentation(output_link)
-
-
-# print("it should be able to take in a template as an argument and then use that template to create the presentation")
-# # it should be able to take in a template as an argument and then use that template to create the presentation
-# output_link = create_presentation(dictionary["slide_data"], template="x_template.pptx")
-
-# it should create a presentation from a template when provided
-
-# output_link = create_presentation(dictionary["slide_data"], template="x_template.pptx")
-# print(output_link)
-# print("it should create a presentation from a template when provided")
 new_schema = {
   "slide_data": [
       {
@@ -138,32 +23,8 @@ new_schema = {
   ]
 }
 
-# def create_presentation_new(slide_data):
-#     presentation = Presentation()
-#     index = 0
-#     for slide in slide_data:
-        
-#         heading = slide.get("heading", "")
-#         content = slide.get("content", [])
-#         layout = None
-#         if index == 0:
-#           layout = presentation.slide_layouts[2]  
-#         else:
-#           layout = presentation.slide_layouts[1]
-#         slide = presentation.slides.add_slide(layout)
-#         title = slide.shapes.placeholders[0].text_frame
-#         title.text = heading
-        
-#         content_holder = slide.shapes.placeholders[1].text_frame
-
-#         for paragraph in content:
-#             content_holder.add_paragraph().text = paragraph
-
-#     # create a filepath for the presentation
-#     filepath = "./presentations/presentation.pptx"
-#     presentation.save(filepath)
-
-
-create_presentation_new(new_schema)
+slide_data =new_schema['slide_data']
+presentation_file = create_presentation_new(slide_data, "./presentations")
+print(presentation_file)
 
     
--- utils.py
+++ utils.py
@@ -2,12 +2,20 @@ from pptx import Presentation
 import uuid
 import os
 
-def create_presentation_new(slide_data, PRESENTATION_DIR):
+def create_presentation_new(slide_data, PRESENTATION_DIR, template_id="plain_template.pptx"):
         # Generate a unique identifier for this presentation
     presentation_id = str(uuid.uuid4())
     # Create a file path for the presentation
     presentation_path = os.path.join(PRESENTATION_DIR, f"{presentation_id}.pptx")
-    presentation = Presentation()
+
+    presentation = None
+    # if there is a template provided, use it
+    if template_id:
+        template_path = os.path.join("./templates", template_id)
+        print(f"using template {template_path}")
+        presentation = Presentation(template_path)
+    else:
+        presentation = Presentation()
     # slide_data = input_data["slide_data"]
     index = 0
     for slide in slide_data:
@@ -28,6 +36,9 @@ def create_presentation_new(slide_data, PRESENTATION_DIR):
         for paragraph in content:
             content_holder.add_paragraph().text = paragraph
         index += 1
+    
+    # delete the first slide
+    # presentation.slides[0].delete()
 
     presentation.save(presentation_path)
     return presentation_path
