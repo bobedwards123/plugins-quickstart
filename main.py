9d06c3b92a58251fb5fb4af4eeb1d39e88a10737
leolukaz123 <122065825+leolukaz123@users.noreply.github.com>
2023-06-26 22:11:01 +1000

    updated it to a pre-signed url

--- main.py
+++ main.py
@@ -4,22 +4,46 @@ import quart_cors
 from quart import request, send_file, jsonify, send_from_directory
 from utils import create_presentation_new
 import os
+import boto3
 
 app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
 
 # Directory to store generated presentations
 PRESENTATION_DIR = "./presentations"
 TEMPLATE_DIR = "./templates"
+S3_BUCKET_NAME = "gptslidesbucket"
+
+# @app.post("/presentation/link")
+# async def presentation_link_new():
+#     slide_data = (await request.get_json())['slide_data']
+#     presentation_file = create_presentation_new(slide_data, PRESENTATION_DIR)
+#     print(presentation_file)
+    
+#     # Generate a unique download link for the file
+#     download_link = f"http://{request.host}/presentation/download/{os.path.basename(presentation_file)}"
+#     return jsonify({"download_link": download_link})
+
 
 @app.post("/presentation/link")
 async def presentation_link_new():
     slide_data = (await request.get_json())['slide_data']
     presentation_file = create_presentation_new(slide_data, PRESENTATION_DIR)
     print(presentation_file)
+
+    # Upload the file to S3
+    s3 = boto3.client('s3')
+    s3.upload_file(presentation_file, S3_BUCKET_NAME, os.path.basename(presentation_file))
+
+    # Generate a unique download link for the file on S3
+    # url = f"https://s3.amazonaws.com/{S3_BUCKET_NAME}/{os.path.basename(presentation_file)}"
+
+    # Generate a presigned URL for the file on S3
+    url = s3.generate_presigned_url('get_object',
+                                              Params={'Bucket': S3_BUCKET_NAME,
+                                                      'Key': os.path.basename(presentation_file)},
+                                              ExpiresIn=3600)  # URL expires in 1 hour
     
-    # Generate a unique download link for the file
-    download_link = f"http://{request.host}/presentation/download/{os.path.basename(presentation_file)}"
-    return jsonify({"download_link": download_link})
+    return jsonify({"download_link": url})
 
 @app.get('/presentation/download/<presentation_id>')
 async def presentation_download(presentation_id):
